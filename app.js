63834fc35016ece99a9756aeca57e36e764f2c44
ZYanets <ZYanets@yandex.ru>
2022-01-29 00:55:33 +0900

    fix: исправить замечание

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
